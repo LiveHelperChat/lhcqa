eec1237cd7e6f12a59cc44129b9657668cb27d71
Remigijus Kiminas <remdex@gmail.com>
2026-07-13 11:55:50 +0000

    Auto reload value

--- design/lhcqatheme/tpl/lhchat/lists_chats_parts/column_end_row_multiinclude.tpl.php
+++ design/lhcqatheme/tpl/lhchat/lists_chats_parts/column_end_row_multiinclude.tpl.php
@@ -1,5 +1,5 @@
 <?php if ($qaCanFill || $qaCanSeeDetailed) : ?>
-<td>
+<td id="qa-score-<?php echo $chat->id;?>">
 <?php if (isset($chat->chat_variables_array['qa_chat_score']) && !$qaCanFillPrivate) : ?>
     <?php if ($qaCanSeeDetailed) : ?>
         <button onclick="lhc.revealModal({'url':WWW_DIR_JAVASCRIPT+'qaform/seedetails/<?php echo $chat->id?>'})" class="btn btn-xs bg-success-subtle" type="button"><?php echo htmlspecialchars($chat->chat_variables_array['qa_chat_score']);?>%</button>
@@ -8,7 +8,7 @@
     <?php endif; ?>
 <?php endif;?>
 <?php if ($qaCanFillPrivate && $qaCanFill) : ?>
-&nbsp;<button onclick="lhc.revealModal({'url':WWW_DIR_JAVASCRIPT+'qaform/fill/<?php echo $chat->id?>'})" type="button" class="btn btn-xs <?php if (isset($chat->chat_variables_array['qa_chat_score'])) :?>bg-success-subtle<?php else : ?>bg-light<?php endif;?>"><?php if (isset($chat->chat_variables_array['qa_chat_score'])) :?><?php echo htmlspecialchars($chat->chat_variables_array['qa_chat_score']);?>%&nbsp;<?php endif;?>QA</button>
+<button onclick="lhc.revealModal({'url':WWW_DIR_JAVASCRIPT+'qaform/fill/<?php echo $chat->id?>'})" type="button" class="btn btn-xs <?php if (isset($chat->chat_variables_array['qa_chat_score'])) :?>bg-success-subtle<?php else : ?>bg-light<?php endif;?>"><?php if (isset($chat->chat_variables_array['qa_chat_score'])) :?><?php echo htmlspecialchars($chat->chat_variables_array['qa_chat_score']);?>%&nbsp;<?php endif;?>QA</button>
 <?php endif; ?>
 </td>
 <?php endif; ?>
